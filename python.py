4af86e6e51c49cf2144ef10d512482906f39dba9
Izyan Khan <izyankhan@Izyans-MacBook-Pro.local>
2026-06-26 08:15:02 -0700

    added some new lines using for loop and while loop

--- python.py
+++ python.py
@@ -1,10 +1,17 @@
 name = input("What is your name? :")
 print(name)
 print(f"Hello {name} Welcome to my python program where i test some code sometimes.")
-age = input("How old are you?")
+age = input("How old are you? :")
 print(f"Hello {name} you are {age} years old")
 guess = input("Try guessing the password :")
 while guess != "Python Program":
     print("That is incorrect please try again")
     guess = input("Try guessing the password :")
 print("That is correct!")
+flavors = ["Ranch"]
+flavors.append("Chicken")
+flavors.append("Margarita")
+for i in range(len(flavors)):
+    print(i + 1, flavors[i])
+choice = input("What flavor do you like? :")
+print(f"{choice} is a great flavor")
