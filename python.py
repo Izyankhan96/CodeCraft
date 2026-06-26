da982e8e8ddb7ae80c8450eed20d43e3b76b6464
Izyan Khan <izyankhan@Izyans-MacBook-Pro.local>
2026-06-26 10:51:02 -0700

    added a new lines of code using variables and while loop

--- python.py
+++ python.py
@@ -15,3 +15,12 @@ for i in range(len(flavors)):
     print(i + 1, flavors[i])
 choice = input("What flavor do you like? :")
 print(f"{choice} is a great flavor")
+choice2 = input("Pick an Ice cream flavor :")
+flavors2 = ["Vanilla"]
+flavors2.append("Chocolate")
+flavors2.append("Mango")
+print(flavors2)
+while choice2 not in flavors2:
+    print("Sorry we dont have that flavor please try again")
+    choice2 = input("Pick an Ice cream flavor :")
+print(f"{choice2} is a great flavor")
