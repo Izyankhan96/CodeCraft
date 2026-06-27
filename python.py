18e9b4edb769d8d451e7cc12b5547a14b94ef41a
Izyan Khan <izyankhan@MacBookPro.lan>
2026-06-27 07:14:25 -0700

    made some changes to the code

--- python.py
+++ python.py
@@ -16,12 +16,12 @@ for i in range(len(flavors)):
 choice = input("What flavor do you like? :")
 print(f"{choice} is a great flavor")
 choice2 = input("Tell me an Ice cream flavor :")
-flavors2 = ["Vanilla"]
-flavors2.append("Chocolate")
-flavors2.append("Mango")
-for i in range(len(flavors2)):
-    print(i + 1, flavors2[i])
-while choice2 not in flavors2:
+icecream = ["Vanilla"]
+icecream.append("Chocolate")
+icecream.append("Mango")
+for i in range(len(icecream)):
+    print(i + 1, icecream[i])
+while choice2 not in icecream:
     print("Sorry we dont have that flavor please try again")
     choice2 = input("Pick an Ice cream flavor :")
 print(f"{choice2} is a great flavor")
