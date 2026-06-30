2d7af24e0c8e31acfa1e37a48a298cb0c0af4072
Izyan Khan <izyankhan@MacBookPro.lan>
2026-06-30 09:51:48 -0700

    Implement Tests

--- Python Testing code.py
+++ Python Testing code.py
@@ -1,9 +1,14 @@
 name = input("Hello Welcome to the grade checker what is your name? :")
 print(f"Hello, {name} I see your name in our system")
-password = input("What is your password :")
+Id_number = input("Please provide your grade checking number :")
+while Id_number != "1234566":
+    print("That is incorrect please Try again :")
+    Id_number = input("Please provide your grade checking number :")
+print("Alright! Please enter your password below")
+password = input("Password :")
 while password != "TheGradeChecker71":
     print("That is not correct Please re-enter your password :")
-    password = input("What is your password")
+    password = input("Password :")
 print(f"That is correct Welcome back {name}")
 print("Here is your grade :")
 grade = 96
