6d8bfe033dea3863bf891801e6f09e311d057b56
Izyan Khan <izyankhan@MacBookPro.lan>
2026-07-16 17:02:00 -0700

    Implement Tests

--- Python Testing code.py
+++ Python Testing code.py
@@ -1,20 +1,26 @@
-name = input("Hello Welcome to the grade checker what is your name? :")
-print(f"Hello, {name} I see your name in our system")
-Id_number = input("Please provide your grade checking number :")
-while Id_number != "1234566":
-    print("That is incorrect please Try again :")
-    Id_number = input("Please provide your grade checking number :")
-print("Alright! Please enter your password below")
-password = input("Password :")
-while password != "TheGradeChecker71":
-    print("That is not correct Please re-enter your password :")
-    password = input("Password :")
-print(f"That is correct Welcome back {name}")
-print("Here is your grade :")
-grade = 96
-if grade >= 90:
-    print("A")
-elif grade >= 80:
-    print("B")
-else:
-    print("C")
+colors = ["Red","Blue","Green","Yellow","Orange","Purple"]
+colors.append("Pink")
+colors.append("Silver")
+colors.append("Gold")
+colors.append("White")
+colors.append("Black")
+colors.append("Brown")
+colors.append("Gray")
+colors.append("Cyan")
+colors.append("Magenta")
+colors.append("Teal")
+colors.append("Maroon")
+colors.append("Navy")
+colors.append("Olive Green")
+for i in range(len(colors)):
+    print(i + 1, colors[i])
+question2 = input("What is your favorite color? :")
+while question2 not in colors:
+    print("Sorry please choose a color from the given options")
+    question2 = input("What is your favorite color? :")
+print(f"{question2} is a great color")
+cars = ["Mazda","Toyota","Tesla","Honda","Hyundai","Lexus","BYD"]
+for i in range(len(cars)):
+    print(i + 1, cars[i])
+question3 = input("Whats your favorite car choose from the options given below :")
+print(f"{question3}, is a great car")
